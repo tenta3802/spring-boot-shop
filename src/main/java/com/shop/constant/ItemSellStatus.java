e9b4ed8c2aee3ee9e53072e6197ba20b9f1e1834
tenta <eunoia3802@gmail.com>
2022-09-09 15:55:10 +0900

    상품 entity 설계

---
+++ src/main/java/com/shop/constant/ItemSellStatus.java
@@ -0,0 +1,6 @@
+package com.shop.constant;
+
+// enum class 란 열거형을 의미
+public enum ItemSellStatus {
+    SELL, SOLD_OUT
+}
---
+++ src/main/java/com/shop/entity/Item.java
@@ -0,0 +1,41 @@
+package com.shop.entity;
+
+import com.shop.constant.ItemSellStatus;
+import lombok.Data;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="item")
+@Data
+@ToString
+public class Item {
+
+    @Id
+    @Column(name="item_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String itemNm;
+
+    @Column(name = "price", nullable = false)
+    private int price;
+
+    @Column(nullable = false)
+    private int stockNumber;
+
+    @Lob
+    @Column(nullable = false)
+    private  String itemDetail;
+
+    @Enumerated(EnumType.STRING)
+    @Column
+    private ItemSellStatus itemSellStatus;
+    // constant.enum 클래스에 저장된 SOLD, SOLD_OUT 을 사용
+
+    private LocalDateTime regTime; // 등록 시간
+    private LocalDateTime updateTime;
+}
