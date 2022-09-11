199f6d6cd334daa4231d38614bed767f3bd67a74
tenta <eunoia3802@gmail.com>
2022-09-11 16:21:31 +0900

    Querydsl - RepositoryTest 완료

--- src/test/java/com/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -1,13 +1,18 @@
 package com.shop.repository;
 
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.shop.constant.ItemSellStatus;
 import com.shop.entity.Item;
+import com.shop.entity.QItem;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -17,6 +22,28 @@ class ItemRepositoryTest {
 
     @Autowired
     ItemRepository itemRepository;
+
+    // 영속성 컨텍스트 사용을 위한 EntityManger bean 주입
+    @PersistenceContext
+    EntityManager em;
+
+    @Test
+    @DisplayName("@Querydsl 조회 테스트")
+    public void queryDslTest(){
+        this.createItemTest();
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+        QItem qItem = QItem.item;
+        JPAQuery<Item> query = queryFactory.selectFrom(qItem)
+                .where(qItem.itemSellStatus.eq(ItemSellStatus.SELL))
+                .where(qItem.itemDetail.like("%" + "테스트 상품 상세 설명" + "%"))
+                .orderBy((qItem.price.desc()));
+
+        List<Item> itemList = query.fetch();
+
+        for (Item item : itemList){
+            System.out.println(item.toString());
+        }
+    }
 
     @Test
     @DisplayName("@Query를 이용한 상품 조회 테스트")
