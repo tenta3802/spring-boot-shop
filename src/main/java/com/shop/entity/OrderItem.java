97d08957336b4d2ac6fab83c7864615dd44ac35f
tenta <eunoia3802@gmail.com>
2022-09-19 18:23:07 +0900

    주문-주문 상품 엔티티 연관관계 설정

---
+++ src/main/java/com/shop/entity/OrderItem.java
@@ -0,0 +1,30 @@
+package com.shop.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter
+public class OrderItem {
+
+    @Id @GeneratedValue
+    @Column(name = "order_item_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    private int count;
+
+    private LocalDateTime regTime;
+
+    private LocalDateTime updateTime;
+}
